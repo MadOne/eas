e1abc489bebbfc016e745cf1a60aae8c9f878ad3
MadOne <madone85@googlemail.com>
2023-11-18 19:20:47 +0100

    Problem set 1-5 solved

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 // this
+
+function createSquares(number){
+    let container = document.querySelector(".container");
+    let newdiv;
+    let dimension = 960 / Math.sqrt(number);
+    for (let i = 0; i < number; i++){
+        newdiv = document.createElement("div");
+        newdiv.classList.add("square");
+        newdiv.style.width = `${dimension}px`;
+        newdiv.style.height = `${dimension}px`;
+        newdiv.id = i;
+        container.append(newdiv);
+    }
+}
+
+function clear() {
+    let container = document.querySelector(".container");
+    container.innerHTML = "";
+}
+
+
+let button = document.querySelector(".reset");
+button.addEventListener('click', (event) => {
+    clear();
+    let size = prompt("How many squares per axis do you want?");
+    createSquares(size * size);
+});
+
+
+
+let container = document.querySelector(".container");
+container.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = "black";
+});
+
+createSquares(16*16);
